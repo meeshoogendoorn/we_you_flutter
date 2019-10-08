c44f753df1c559cb3a24a3c3804e2284e1e0216d
AMKBusiness <mees@amkbusiness.nl>
2019-10-08 18:09:47 +0200

    Added DataSource for REST api

---
+++ lib/data/restDataSource.dart
@@ -0,0 +1,26 @@
+import 'dart:async';
+import 'dart:convert';
+
+import 'package:survey_app/utils/network.dart';
+import 'package:survey_app/models/user.dart';
+
+class RestDataSource {
+  // defining constant variables for authentication
+  NetworkUtil _netUtil = new NetworkUtil();
+  static final BASE_URL = 'http://dev.meeshoogendoorn.nl/api';
+  static final AUTH_URL = BASE_URL + "/accounts/login/";
+  //TODO == API token based
+
+  // handler for login request
+  Future<String> login(String username, String password) {
+    return _netUtil.post(AUTH_URL, headers: {
+      "authorization": 'Basic ' + base64Encode(utf8.encode('$username:$password'))
+    }).then((dynamic res){
+      print(res.toString());
+//      if(res["detail"]) {
+//        throw new Exception(res["detail"]);
+//      }
+      return res["token"].toString();
+    });
+  }
+}
