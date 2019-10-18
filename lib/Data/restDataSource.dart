6550c94e793ee20667c827bd75e98c5a5aaca9e4
AMKBusiness <mees@amkbusiness.nl>
2019-10-18 09:53:02 +0200

    Created DataSource for Post and Get request to API

--- lib/Data/restDataSource.dart
+++ lib/Data/restDataSource.dart
@@ -4,18 +4,19 @@ import 'dart:convert';
 import 'package:survey_app/utils/network.dart';
 
 class RestDataSource {
+  static const BASE_URL = 'http://dev.meeshoogendoorn.nl/api';
+  static const AUTH_URL = BASE_URL + "/accounts/login/";
+
   // defining constant variables for authentication
   NetworkUtil _netUtil = new NetworkUtil();
-  static final BASE_URL = 'http://dev.meeshoogendoorn.nl/api';
-  static final AUTH_URL = BASE_URL + "/accounts/login/";
   //TODO == API token based
 
   // handler for login request
   Future<String> login(String username, String password) {
     return _netUtil.post(AUTH_URL, headers: {
-      "authorization": 'Basic ' + base64Encode(utf8.encode('$username:$password'))
+      "Authorization": 'Basic ' + base64Encode(utf8.encode('$username:$password'))
     }).then((dynamic res){
-      print(res.toString());
+//      print(res.toString());
 //      if(res["detail"]) {
 //        throw new Exception(res["detail"]);
 //      }
